07a78f57dfba1bcaabedff26572f216d9258608e
Danilo Justino da Silva <danilo.justino.contabilidade@gmail.com>
2026-09-15 19:17:39 +0000

    finalizado as tabelas fatos para a area de staging

---
+++ models/trusted/erp_banvic/trt_erp_transacoes.sql
@@ -0,0 +1,20 @@
+with
+    fonte_transacoes as (
+        select *
+        from {{ source('erp', 'transacoes') }}
+    )
+
+    , renomeado as (
+        select
+            Cast(cod_transacao as int) as pk_transacao
+            , cast(num_conta as int) as fk_conta
+            , cast(data_transacao as timestamp) as ts_data_transacao
+            , nome_transacao
+            , cast(valor_transacao as numeric(32, 2)) as valor_transacao
+        from fonte_transacoes
+    )
+
+
+select *
+from renomeado
+
